a3ffd9586ecc0e7a3db33ad88e2a086191dc26f6
Mark Fynes <tfynes@vmware.com>
2024-02-13 16:47:09 -0500

    .

--- src/main/java/com/fynesy/si1/Si1Application.java
+++ src/main/java/com/fynesy/si1/Si1Application.java
@@ -2,12 +2,21 @@ package com.fynesy.si1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Si1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Si1Application.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("hello world");
+		return "hello world\n";
 	}
 
 }
