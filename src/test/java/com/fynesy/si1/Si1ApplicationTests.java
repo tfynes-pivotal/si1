b6d030549602a5fd80831a7c42957ec76630e7b9
Mark Fynes <tfynes@vmware.com>
2024-02-13 16:31:15 -0500

    ...

---
+++ src/test/java/com/fynesy/si1/Si1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fynesy.si1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
